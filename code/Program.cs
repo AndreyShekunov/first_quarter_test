817cac79339c21fe4cd52990f2dcbd10a44d1721
Шекунов Андрей <asheknov@gmail.com>
2022-10-14 19:45:30 +0500

    Добавил методы: ввод числа, создание массива пользователем, вывод массива в терминал

--- code/Program.cs
+++ code/Program.cs
@@ -7,3 +7,39 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ["1234", "1567", "-2", "competer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+// метод ввода числа
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// метод создания и заполнения пользователем исходного массива
+string[] GetArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива:");
+        array[i] = Console.ReadLine() ?? "";
+    }
+    return array;
+}
+
+// метод вывода массива в терминал
+void PrintArray(string[] array) 
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"'{array[i]}' ");
+    }
+    Console.WriteLine();
+}
+
+int sizeArray = ReadNumber("Введите размер создаваемого массива:");
+string[] array = GetArray(sizeArray);
+PrintArray(array);
